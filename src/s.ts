dc19f218a13588e035bc2e74f226a358661f60cb
marcin najder <marcin.najder@comarch.pl>
2017-03-28 23:20:17 +0200

    fix s.dump method (now it is not modifying passed data)

--- src/s.ts
+++ src/s.ts
@@ -1,4 +1,4 @@
-// import * as path from "path";
+// this file can be loaded from mongo shell environment
 namespace s {
     var maxColumnWidth = 30;
     var spaces = "                                                         ";
@@ -6,23 +6,25 @@ namespace s {
 
     // Overload signatures must all be ambient or non-ambient.
     export function dump(data) {
-        var keys, value, headers = {};
-        var str = "";
+        var keys, value, headers = {}, newData = [], newRow, str = "";
 
         if (!Array.isArray(data)) {
             data = [data];
         }
 
+        // figure out header (first row of the table)
         for (var row of data) {
             keys = Object.keys(row || {});
+            newRow = {};
 
             for (var key of keys) {
+
                 value = row[key];
 
                 if (value === null) {
                     value = "null";
                 }
-                else if (value instanceof ObjectId) {
+                else if (typeof ObjectId !== "undefined" && value instanceof ObjectId) {
                     value = value.str;
                 }
                 else if (value === undefined) {
@@ -31,9 +33,9 @@ namespace s {
                 else if (value instanceof Date) {
                     value = value.toISOString();
                 }
-                else if (Array.isArray(value)) {
+                else if (Array.isArray(value) || isObject(value)) {
                     continue;
-                    // pomijamy poki co tablice
+                    // for now skip objects and arrays
                 }
                 else {
                     value = value.toString();
@@ -42,15 +44,18 @@ namespace s {
                 if (value.length > maxColumnWidth) {
                     value = value.substring(0, maxColumnWidth) + "...";
                 }
-                row[key] = value;
                 headers[key] = Math.max(headers[key] || 0, value.length, key.length);
+                newRow[key] = value;
             }
+            newData.push(newRow);
         }
-
+        // display head
         str += "documents: " + data.length + nl;
         str += Object.keys(headers).map(h => padRight(h, headers[h])).join(" | ") + nl;
         str += Object.keys(headers).map(h => padRight("-".repeat(headers[h]), headers[h])).join(" | ") + nl;
-        for (var row of data) {
+
+        // display data
+        for (var row of newData) {
             str += Object.keys(headers).map(h => padRight(row[h] || "", headers[h])).join(" | ") + nl;
         }
 
@@ -64,7 +69,16 @@ namespace s {
     function padRight(text, count) {
         return text + spaces.substring(0, count - text.length);
     }
+
+    /**https://github.com/jashkenas/underscore/blob/master/underscore.js */
+    function isObject(obj) {
+        var type = typeof obj;
+        return type === 'function' || type === 'object' && !!obj;
+    }
 }
 
-
-
+// sample test, just execute "node ./dist/src/s.js""
+// var obj = { name: "john", age: 123, adress: ["Berlin", "Warsaw"], jon: { name: "IBM" } };
+// console.log(obj);
+// console.log(s.dump(obj));
+// console.log(obj);
